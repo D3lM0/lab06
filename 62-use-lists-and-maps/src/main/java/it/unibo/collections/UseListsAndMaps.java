c39ab33a44f2c619527d1c7abe3b21b79a758c0b
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-27 11:27:56 +0100

    Got OOP-git-merge-conflivt-test from git in 61 exercises, Added .gitignore file to 62 exercises, Created Integer ArrayList and LinkedList

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,7 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,16 +23,26 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> integerArrayList = new ArrayList<>();
+        for (int i = 1000; i <= 2000; i++) {
+            integerArrayList.add(i);
+        }       
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        List<Integer> integerLinkedList = new LinkedList<>(integerArrayList);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        int firstElementArrayList = integerArrayList.get(0);
+        int lastElementArrayList = integerArrayList.get(integerArrayList.size());
+
+        integerArrayList.set(0, lastElementArrayList);
+        
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
