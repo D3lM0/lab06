9569e2ee9fac152bb97849cbf9f9c25a38901fa0
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-28 11:03:41 +0100

    Done catch existing exceptions (63-exceptions)

--- 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -48,6 +49,16 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        boolean sent = false;
+        
+        while (!sent) {
+            try {
+                server.sendData(message);
+                sent = true;
+            } catch (final IOException e) {
+                LOG.println("Network error while sending. Retrying... " + e.getMessage());
+            }
+        }
     }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
@@ -55,7 +66,13 @@ public final class UseArithmeticService {
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-        return null;
+        while (true) {
+            try {
+                return server.receiveResponse();
+            } catch (final IOException e) {
+                LOG.println("Network error while receiving. Retrying... " + e.getMessage());
+            }
+        }
     }
 
     private static void assertEqualsAsDouble(final String expected, final String actual) {
