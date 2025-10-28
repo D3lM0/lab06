13ef3584247ec9e096857e2823eef8b4fca1630f
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-28 16:42:15 +0100

    Implemented SocialnetworkUserImpl (64-design-with-maps)

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -11,10 +11,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * This will be an implementation of
@@ -38,6 +36,7 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *
      * think of what type of keys and values would best suit the requirements
      */
+    private Map<String, List<U>> followedUsers;
 
     /*
      * [CONSTRUCTORS]
@@ -64,13 +63,16 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
+        followedUsers = new HashMap<>();
     }
 
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
-
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+        super(name, surname, user, -1);
+    }
     /*
      * [METHODS]
      *
@@ -78,7 +80,8 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        followedUsers.putIfAbsent(circle, new ArrayList<>());
+        return followedUsers.get(circle).add(user);
     }
 
     /**
@@ -88,11 +91,15 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+        return followedUsers.getOrDefault(groupName, Collections.emptyList());
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+        ArrayList<U> followedUsersList = new ArrayList<>();
+        for (List<U> group : followedUsers.values()) {
+            followedUsersList.addAll(group);
+        }
+        return followedUsersList;
     }
 }
