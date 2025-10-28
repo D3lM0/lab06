e2d58172400e9cc24820b7f478fdce03e27c6f85
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-28 16:52:25 +0100

    Completed test class

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/TestSocialNetworkUser.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/TestSocialNetworkUser.java
@@ -51,10 +51,11 @@ public final class TestSocialNetworkUser {
          *
          * * Adam Smith, asmith, (no age)
          */
-        final SocialNetworkUser<User> kbacon = null; //TODO
-        final SocialNetworkUser<User> dwashington = null; //TODO
-        final SocialNetworkUser<User> mgladwell = null; //TODO
-        final SocialNetworkUser<User> ntaleb = null; //TODO
+        final SocialNetworkUser<User> kbacon = new SocialNetworkUserImpl<>("Kevin", "Bacon", "kbacon", 56); // TODO
+        final SocialNetworkUser<User> dwashington = new SocialNetworkUserImpl<>("Denzel", "Washington", "dwashington",
+                59); // TODO
+        final SocialNetworkUser<User> mgladwell = new SocialNetworkUserImpl<>("Malcom", "Gladwell", "mgladwell", 51); // TODO
+        final SocialNetworkUser<User> ntaleb = n; // TODO
         final User asmith = null; //TODO
         /*
          * Make people follow each other
