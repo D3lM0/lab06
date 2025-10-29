7a559e5b74c4bb258dbd6be640a3724bc1e7fb82
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-29 16:55:59 +0100

    Implemented method getPath (65-implement-generic-class)

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,9 +1,13 @@
 package it.unibo.generics.graph.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
@@ -21,7 +25,7 @@ public class GraphImpl<N> implements Graph<N> {
     }
 
     @Override
-    public void addNode(N node) {
+    public void addNode(final N node) {
         if (!this.nodes.contains(node) && node != null) {
             this.nodes.add(node);
             this.edges.putIfAbsent(node, new HashSet<>());
@@ -29,7 +33,7 @@ public class GraphImpl<N> implements Graph<N> {
     }
 
     @Override
-    public void addEdge(N source, N target) {
+    public void addEdge(final N source, final N target) {
         if (source != null && target != null && nodes.contains(source) && nodes.contains(source)) {
             edges.get(source).add(target);
         }
@@ -41,13 +45,41 @@ public class GraphImpl<N> implements Graph<N> {
     }
 
     @Override
-    public Set<N> linkedNodes(N node) {
+    public Set<N> linkedNodes(final N node) {
         return edges.get(node);
     }
 
     @Override
-    public List<N> getPath(N source, N target) {
-        return null;
+    public List<N> getPath(final N source, final N target) {
+        /* BFS implementation */
+        final Map<N, N> predecessors = new HashMap<>();
+        final Queue<N> queue = new LinkedList<>();
+
+        queue.add(source);
+        predecessors.put(source, null);
+
+        while (!queue.isEmpty()) {
+            final N current = queue.poll();
+            if (current.equals(target)) {
+                return buildPath(predecessors, target);
+            }
+            for (final N neighbour : edges.get(current)) {
+                if (!predecessors.containsKey(neighbour)) {
+                    predecessors.put(neighbour, current);
+                    queue.add(neighbour);
+                }
+            }
+        }
+        return List.of();
+    }
+
+    private List<N> buildPath(final Map<N, N> predecessors, final N target) {
+        final List<N> path = new ArrayList<>();
+        for (N step = target; step != null; step = predecessors.get(step)) {
+            path.add(step);
+        }
+        Collections.reverse(path);
+        return path;
     }
 
 }
