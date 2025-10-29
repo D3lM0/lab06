c3bddd8c0b32189324fac8683f86ef48186ad090
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-10-29 16:31:42 +0100

    Added GraphImpl file and implemented methods: addNode(), addEdge(), nodeSet() (65-implement-generic-class)

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -0,0 +1,41 @@
+package it.unibo.generics.graph.impl;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import it.unibo.generics.graph.api.Graph;
+
+public class GraphImpl<N> implements Graph<N> {
+    /*Node set */
+    private final Set<N> nodes;
+    
+    /*Adjacency map: for each node, the set of all the reacheble nodes */
+    private final Map<N, Set<N>> edges;
+
+    public GraphImpl() {
+        this.nodes = new HashSet<>();
+        this.edges = new HashMap<>();
+    }
+
+    @Override
+    public void addNode(N node) {
+        if (!this.nodes.contains(node) && node != null) {
+            this.nodes.add(node);
+            this.edges.putIfAbsent(node, new HashSet<>());
+        }
+    }
+
+    @Override
+    public void addEdge(N source, N target) {
+        if (source != null && target != null && nodes.contains(source) && nodes.contains(source)) {
+            edges.get(source).add(target);
+        }
+    }
+
+    @Override
+    public Set<N> nodeSet() {
+        return Set.copyOf(nodes);
+    }
+}
